ad192289d6b1f0bac2231e7607135e6b18fd48ef
Kevin <kesult@ttu.ee>
2020-12-12 13:40:56 +0200

    add edit/delete actions and action column to table. empty functions

--- src/app/components/question-set/question-set.component.ts
+++ src/app/components/question-set/question-set.component.ts
@@ -4,6 +4,7 @@ import {MatChipInputEvent} from '@angular/material/chips';
 import {QuestionService} from '../../services/question.service';
 import {MatTable} from '@angular/material/table';
 import { ActivatedRoute } from '@angular/router';
+import {Question} from '../../question';
 
 export interface Answer {
   text: string;
@@ -15,8 +16,8 @@ export interface Answer {
   styleUrls: ['./question-set.component.css']
 })
 export class QuestionSetComponent implements OnInit {
-  @ViewChild(MatTable) table: MatTable<any>;
-  columnsToDisplay = ['id', 'question', 'answer'];
+  @ViewChild(MatTable) table: MatTable<Question>;
+  columnsToDisplay = ['id', 'question', 'answer', 'action'];
   questionSet: Array<{}> = [];  // Array of objects
   selectable = true;
   removable = true;
@@ -28,12 +29,14 @@ export class QuestionSetComponent implements OnInit {
   constructor(private questionService: QuestionService,
               private route: ActivatedRoute ) { }
   ngOnInit(): void {
+    // Get pathvariables question Set id and name.
     this.route.paramMap.subscribe(params => {
         this.setId = +params.get('id');
         this.setName = params.get('name');
     });
     this.getQuestions();
   }
+  // Add choices under question.
   addAnswer(event: MatChipInputEvent): void {
     const input = event.input;
     const value = event.value;
@@ -46,12 +49,14 @@ export class QuestionSetComponent implements OnInit {
       input.value = '';
     }
   }
+  // Remove one choice from question.
   remove(answer: Answer): void {
     const index = this.choices.indexOf(answer);
     if (index >= 0) {
       this.choices.splice(index, 1);
     }
   }
+  // Add a new question to database, reload the page.
   addQuestion(): void {
     const questionval = this.getInputValueById('question');
     const answerval = this.getInputValueById('answer');
@@ -67,17 +72,29 @@ export class QuestionSetComponent implements OnInit {
       choices: choices1
     };
     console.error(questionObj);
-    this.questionService.postQuestion(questionObj).subscribe(quest => this.table.renderRows());
+    this.questionService.postQuestion(questionObj).subscribe(() => {
+      console.log('Hey im here');
+      this.table.renderRows();
+    });
     window.document.location.reload();
-  }
-  private getQuestions(): void {
-    this.questionService.getQuestions(this.setId)
-      .subscribe(questions => this.questionSet = questions);
   }
   emptyChoices(): void {
     this.choices = [];
   }
   private getInputValueById(id: string): string {
     return (document.getElementById(id) as HTMLInputElement).value;
+  }
+  // Get all questions for this question set, meant for table.
+  private getQuestions(): void {
+    this.questionService.getQuestions(this.setId)
+      .subscribe(questions => this.questionSet = questions);
+  }
+  // Remove a question completely from database.
+  public deleteQuestion(id: number): void {
+
+  }
+  // Change question field values.
+  editQuestion(id: number): void {
+    console.log(id);
   }
 }
