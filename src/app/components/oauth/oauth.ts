9669953b14b23356fd28a5613ee8a0c4412fe6c9
Robin-Kevin Koppa <rokopp@ttu.ee>
2021-02-05 18:21:42 +0200

    Changed redirect url

--- src/app/components/oauth/oauth.ts
+++ src/app/components/oauth/oauth.ts
@@ -1,6 +1,7 @@
 export const OAuthSettings = {
   appId: '78637df6-bd9c-4013-9a4e-e935736bcf92',
-  redirectUri: 'http://localhost:8082/login/oauth2',
+  // redirectUri: 'http://localhost:8082/login/oauth2/code/azure',
+  redirectUri: 'https://skoop.cs.taltech.ee/login/oauth2/code/azure',
   tenantID: 'https://login.microsoftonline.com/3efd4d88-9b88-4fc9-b6c0-c7ca50f1db57',
   scopes: [
     'openid',
