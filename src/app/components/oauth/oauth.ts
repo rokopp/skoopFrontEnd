b6a95da5064e453e2a5047c628468699c9500a14
Robin-Kevin Koppa <rokopp@ttu.ee>
2021-02-05 16:21:38 +0200

    Testing OAuth

--- src/app/components/oauth/oauth.ts
+++ src/app/components/oauth/oauth.ts
@@ -1,6 +1,6 @@
 export const OAuthSettings = {
   appId: '3efd4d88-9b88-4fc9-b6c0-c7ca50f1db57',
-  redirectUri: 'http://localhost:8082/login',
+  redirectUri: 'https://skoop.cs.taltech.ee/login/oauth2/code/azure',
   scopes: [
     'openid',
     'user.read',
