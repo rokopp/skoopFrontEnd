85dcf3b0d55ef1d5d1267498e845d085f6b7ce7f
Robin-Kevin Koppa <rokopp@ttu.ee>
2021-02-05 17:45:05 +0200

    Changed Client ID

--- src/app/components/oauth/oauth.ts
+++ src/app/components/oauth/oauth.ts
@@ -1,5 +1,5 @@
 export const OAuthSettings = {
-  appId: '3efd4d88-9b88-4fc9-b6c0-c7ca50f1db57',
+  appId: '78637df6-bd9c-4013-9a4e-e935736bcf92',
   redirectUri: 'http://localhost:8082/login/oauth2',
   scopes: [
     'openid',
