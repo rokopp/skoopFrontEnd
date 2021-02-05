2275101cb4987ab35e2532b631bd742c3b711e36
Robin-Kevin Koppa <rokopp@ttu.ee>
2021-02-05 17:34:53 +0200

    Testing OAuth

--- src/app/components/oauth/oauth.ts
+++ src/app/components/oauth/oauth.ts
@@ -1,12 +1,10 @@
 export const OAuthSettings = {
-  appId: '78637df6-bd9c-4013-9a4e-e935736bcf92',
-  redirectUri: 'https://skoop.cs.taltech.ee/api/login',
-  authority: '3efd4d88-9b88-4fc9-b6c0-c7ca50f1db57', // This is your tenant ID
+  appId: '3efd4d88-9b88-4fc9-b6c0-c7ca50f1db57',
+  redirectUri: 'http://localhost:8082/login/oauth2',
   scopes: [
     'openid',
     'user.read',
     'profile'
   ]
 };
-
 // oauth
