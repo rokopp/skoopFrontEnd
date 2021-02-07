6a66d57ecdb79b3982f80e7d6dde0de3f7e4c878
Robin-Kevin Koppa <rokopp@ttu.ee>
2021-02-07 18:40:25 +0200

    Minor fix

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -13,6 +13,7 @@ import {UsersRoomsComponent} from './components/users-rooms/users-rooms.componen
 import {LoginComponent} from './components/login/login.component';
 import {QuestionSetComponent} from './components/question-set/question-set.component';
 import {QuestionSetsComponent} from './components/question-sets/question-sets.component';
+import { MsalGuard } from '@azure/msal-angular/msal.guard';
 
 const routes: Routes = [
   { path: '', redirectTo: '/avaleht', pathMatch: 'full'},
@@ -32,6 +33,7 @@ const routes: Routes = [
   { path: 'questionsets/:id/:name', component: QuestionSetComponent},
   { path: 'questionsets', component: QuestionSetsComponent},
   { path: 'login', component: LoginComponent },
+  { path: 'login/oauth2/code/azure', redirectTo: '/avaleht'}
 ];
 
 const isIframe = window !== window.parent && !window.opener;
