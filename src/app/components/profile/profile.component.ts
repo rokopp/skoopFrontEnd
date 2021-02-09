3a5bd9e98866d7309aa1db1de2617a4cde343d53
Robin-Kevin Koppa <rokopp@ttu.ee>
2021-02-09 15:32:16 +0200

    Acquired token

--- src/app/components/profile/profile.component.ts
+++ src/app/components/profile/profile.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import {HttpClient} from '@angular/common/http';
+import {MsalService} from "@azure/msal-angular";
 
 const GRAPH_ENDPOINT = 'https://graph.microsoft.com/v1.0/me';
 
@@ -12,10 +13,11 @@ export class ProfileComponent implements OnInit {
 
   profile;
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient, private authService: MsalService) { }
 
   ngOnInit(): void {
     this.getProfile();
+    this.authService.acquireTokenPopup({scopes: ['user.read']});
   }
 
   getProfile(): void {
