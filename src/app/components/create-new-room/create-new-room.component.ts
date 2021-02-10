d33b5ce85a110c99c3fb58d3be1d6af2b4bf8de7
Artur Kerb <arkerb@taltech.ee>
2021-02-10 17:46:39 +0200

    Pair generation works on first click now

--- src/app/components/create-new-room/create-new-room.component.ts
+++ src/app/components/create-new-room/create-new-room.component.ts
@@ -1,15 +1,15 @@
 import { Location } from './../../location';
 import { LocationService } from './../../services/location.service';
 import { QuestionService } from './../../services/question.service';
-import {ActivatedRoute} from '@angular/router';
-import {LocationsetsService} from '../../services/locationsets.service';
-import {QuestionsetsService} from '../../services/questionsets.service';
-import {Locationset} from '../../locationset';
-import {Questionset} from '../../questionset';
-import {PairService} from '../../services/pair.service';
-import {Pair} from '../../pair';
-import {Location as URL} from '@angular/common';
-import {Component, OnInit} from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { LocationsetsService } from '../../services/locationsets.service';
+import { QuestionsetsService } from '../../services/questionsets.service';
+import { Locationset } from '../../locationset';
+import { Questionset } from '../../questionset';
+import { PairService } from '../../services/pair.service';
+import { Pair } from '../../pair';
+import { Location as URL } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
 import { Question } from 'src/app/question';
 
 
@@ -37,52 +37,56 @@ export class CreateNewRoomComponent implements OnInit {
               private locationService: LocationService,
               private pairS: PairService,
               private activatedroute: ActivatedRoute,
-              ) {
+  ) {
     this.currentId = this.activatedroute.snapshot.paramMap.get('id');
   }
-
   ngOnInit(): void {
     this.getLocationSets();
     this.getQuestionSets();
   }
-  getQuestionSets(): void{
+  getQuestionSets(): void {
     this.questionSetS.getQuestionSets().subscribe(sets => this.questionSetdata = sets);
   }
-  getLocationSets(): void{
+  getLocationSets(): void {
     this.locationSetS.getLocationSets().subscribe(sets => this.locationSetdata = sets);
   }
-  backClicked(): void{
+  backClicked(): void {
     this.location.back();
   }
-  addPair(): void{
-    if (this.selectedQuestionSet && this.selectedLocationSet){
-      this.questionService.getQuestions(this.selectedQuestionSet).subscribe(questions => this.questions = questions);
-      this.locationService.getLocationsBySet(this.selectedLocationSet).subscribe(locations => this.locations = locations);
+  addPair(): void {
+    if (this.selectedQuestionSet && this.selectedLocationSet) {
       this.minAmount = Math.min(this.questions.length, this.locations.length);
-      if (this.minAmount !== 0){
-        for (let i = 0; i < this.minAmount; i++){
-          this.pairS.postPair({questionId: this.questions[i].id, locationId: this.locations[i].id, roomId: this.currentId} as Pair)
-          .subscribe(() => {
-          },
-            error => {
-              const errorMessage = error.message;
-              console.error('Happened this during posting: ', errorMessage);
-            });
+      if (this.minAmount !== 0) {
+        for (let i = 0; i < this.minAmount; i++) {
+          this.pairS.postPair({ questionId: this.questions[i].id, locationId: this.locations[i].id, roomId: this.currentId } as Pair)
+            .subscribe(() => {
+            },
+              error => {
+                const errorMessage = error.message;
+                console.error('Happened this during posting: ', errorMessage);
+              });
         }
         this.backClicked();
       }
     }
   }
-  deletePairs(): void{
+  updateQuestion(): void {
+    this.questionService.getQuestions(this.selectedQuestionSet).subscribe(questions => this.questions = questions);
+  }
+  updateLocation(): void {
+    this.locationService.getLocationsBySet(this.selectedLocationSet).subscribe(locations => this.locations = locations);
+  }
+
+  deletePairs(): void {
     this.pairS.getPairs().subscribe(pairs => {
       pairs.forEach(pair => {
-        if (pair.roomId.toString() === this.currentId){
+        if (pair.roomId.toString() === this.currentId) {
           this.pairS.removePair(pair.id).subscribe();
           this.succeful = true;
         }
       });
     });
-    if (this.succeful){
+    if (this.succeful) {
       alert('Edukalt Kustutatud');
       this.succeful = false;
     }
