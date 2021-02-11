a55bdbbb618447a4bcc391885e61105621e12c75
karlhe <karlhe@ttu.ee>
2021-02-11 18:46:42 +0000

    update end-point to match back-end

--- src/app/components/oauth/oauth.ts
+++ src/app/components/oauth/oauth.ts
@@ -4,7 +4,7 @@ export const OAuthSettings = {
   // tenantID: 'https://login.microsoftonline.com/3efd4d88-9b88-4fc9-b6c0-c7ca50f1db57',
 
   appId: 'd8f854c2-0de3-4ea6-a62f-8eee48c1b822',
-  redirectUri: 'https://skoop.cs.taltech.ee/api/login/oauth2/authorization/azure',
+  redirectUri: 'https://skoop.cs.taltech.ee/api/login/oauth2/code/azure',
   tenantID: 'https://login.microsoftonline.com/e90ec27d-08af-4b1b-bf3b-5c3c783c2424',
   scopes: [
     'openid',
