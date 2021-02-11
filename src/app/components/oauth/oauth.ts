7820e601c5c4f56867a5bb0a19b23349164d8102
Robin-Kevin Koppa <rokopp@ttu.ee>
2021-02-11 19:18:48 +0200

    Testing OAuth with non taltech

--- src/app/components/oauth/oauth.ts
+++ src/app/components/oauth/oauth.ts
@@ -1,7 +1,11 @@
 export const OAuthSettings = {
-  appId: '78637df6-bd9c-4013-9a4e-e935736bcf92',
-  redirectUri: 'https://skoop.cs.taltech.ee/login/oauth2/code/azure',
-  tenantID: 'https://login.microsoftonline.com/3efd4d88-9b88-4fc9-b6c0-c7ca50f1db57',
+  // appId: '78637df6-bd9c-4013-9a4e-e935736bcf92',
+  // redirectUri: 'https://skoop.cs.taltech.ee/login/oauth2/code/azure',
+  // tenantID: 'https://login.microsoftonline.com/3efd4d88-9b88-4fc9-b6c0-c7ca50f1db57',
+
+  appId: 'd8f854c2-0de3-4ea6-a62f-8eee48c1b822',
+  redirectUri: 'https://skoop.cs.taltech.ee/api/login',
+  tenantID: 'https://login.microsoftonline.com/e90ec27d-08af-4b1b-bf3b-5c3c783c2424',
   scopes: [
     'openid',
     'user.read',
