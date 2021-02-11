213bdb38414e47eae0ec327d88dffafb2d7c9aee
Robin-Kevin Koppa <rokopp@ttu.ee>
2021-02-11 21:40:07 +0200

    Wrong appid

--- src/app/components/oauth/oauth.ts
+++ src/app/components/oauth/oauth.ts
@@ -2,7 +2,7 @@ export const OAuthSettings = {
   // appId: '78637df6-bd9c-4013-9a4e-e935736bcf92',
   // redirectUri: 'https://skoop.cs.taltech.ee/login/oauth2/code/azure',
   // tenantID: 'https://login.microsoftonline.com/3efd4d88-9b88-4fc9-b6c0-c7ca50f1db57',
-  appId: '976c06ed-2d53-4d18-a086-ab04c69ac111',
+  appId: 'd8f854c2-0de3-4ea6-a62f-8eee48c1b822',
   redirectUri: 'https://skoop.cs.taltech.ee/api/login/oauth2/code/azure',
   tenantID: 'https://login.microsoftonline.com/e90ec27d-08af-4b1b-bf3b-5c3c783c2424',
   scopes: [
