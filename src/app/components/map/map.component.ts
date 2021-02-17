88e5be6db13a4bc1f5d58f7c81d27bad347da48b
Artur Kerb <arkerb@taltech.ee>
2021-02-17 12:19:02 +0200

    Marker now works on first click and code is easier to read

--- src/app/components/map/map.component.ts
+++ src/app/components/map/map.component.ts
@@ -10,7 +10,6 @@ import { LocationService } from './../../services/location.service';
 import { FormBuilder, FormGroup} from '@angular/forms';
 import Map from 'ol/Map';
 import OSM from 'ol/source/OSM';
-import { transform, LonLat, LonLatfrom } from 'ol/proj.js';
 import Geolocation from 'ol/Geolocation';
 import Feature from 'ol/Feature';
 import Point from 'ol/geom/Point';
@@ -20,10 +19,10 @@ import {Tile as TileLayer, Vector as VectorLayer} from 'ol/layer';
 import VectorSource from 'ol/source/Vector';
 import {Circle as CircleStyle, Fill, Stroke, Icon, Style} from 'ol/style';
 import { ActivatedRoute } from '@angular/router';
-import {Location as URL} from '@angular/common';
 
 declare let $: any;
 
+// Interface to help with saving marker coordinates
 interface LonAndLat {
   lat: number;
   lng: number;
@@ -46,7 +45,7 @@ export class MapComponent implements OnInit {
   questionobj: Question;
   question;
   answer;
-  choices: String[] = [];
+  choices: string[] = [];
   points;
   map;
   player;
@@ -62,20 +61,26 @@ export class MapComponent implements OnInit {
   positionFeature; // GPS Tracking
   accuracyFeature; // GPS Tracker Radius
   questions$: Observable<Question[]>;
-  constructor(private formBuilder: FormBuilder,
-              private _Activatedroute:ActivatedRoute,
-              private _location: URL,
-              private pairService: PairService,
-              private locationService: LocationService,
-              private questionService: QuestionService
-              ) {
+
+  constructor(
+    private formBuilder: FormBuilder,
+    private activatedRoute: ActivatedRoute,
+    private pairService: PairService,
+    private locationService: LocationService,
+    private questionService: QuestionService
+  ) {
     this.form = this.formBuilder.group({
       answers: ['']
     });
-    this.gameId = this._Activatedroute.snapshot.paramMap.get('id');
+    this.gameId = this.activatedRoute.snapshot.paramMap.get('id');
+  }
+  ngOnInit(): void {
+    this.initializeMap();
+    this.getPairs();
+    this.updateMap();
   }
 
-  public getAnswers(point): any{}
+  // Checks if current questions answer is correct
   public checkanswer(): void{
     console.log(this.selectedAnswer);
     if (this.selectedAnswer.trim() === this.answer.trim()){
@@ -90,7 +95,8 @@ export class MapComponent implements OnInit {
     }
   }
 
-  getlocation(): void{
+  // Get current players location and sets it as map center
+  getPlayerLocation(): void{
     this.accuracyFeature = new Feature();
     this.geolocation.on('change:accuracyGeometry', () => {
     this.accuracyFeature.setGeometry(this.geolocation.getAccuracyGeometry());
@@ -109,6 +115,7 @@ export class MapComponent implements OnInit {
     });
   }
 
+  // Gets all question and location pairs of the current game
   getPairs(): void{
     this.pairService.getPairs().subscribe(pairs =>
       pairs.forEach(pair => {
@@ -119,16 +126,44 @@ export class MapComponent implements OnInit {
         }
       }));
   }
-  getQuestion(id: number): void{
-    this.questionService.getQuestionById(id).subscribe(question =>
-      this.questionobj = question);
+
+  // Get current markers question
+  getQuestion(id: number, feature: Feature): void{
+    this.questionService.getQuestionById(id).subscribe(question => {
+      this.questionobj = question;
+      this.showQuestion(feature);
+    }
+      );
   }
 
+  // Saves all current question data and shows question on the map
+  showQuestion(feature: Feature): void{
+    console.log(this.questionobj);
+    if (this.questionobj !== undefined) {
+      this.question = this.questionobj.question;
+      this.answer = this.questionobj.answer;
+      this.choices = [];
+      this.questionobj.choices.forEach(q => {
+        this.choices.push(q);
+      });
+      this.choices = this.questionobj.choices;
+      this.points = this.questionobj.points;
+      console.log(this.choices);
+      console.log(this.answer);
+      this.showModal();
+      this.amount--;
+      this.vectorSource.removeFeature(feature);
+    }
+  }
+
+  // Function the gets correct coordinates of marker and calls helper function to add them to the map
   addNewLocation(location: Location, qID: number): void{
     this.amount++;
     const coordinates: LonAndLat = JSON.parse(location.location);
     this.createMarekrs(coordinates.lng, coordinates.lat, location.id, qID);
   }
+
+  // Helper function that adds markers on the map
   createMarekrs(Lon, Lat, Id, qID): Feature{
     const marker = new Feature({
       geometry: new Point(fromLonLat([Lon, Lat])),
@@ -146,15 +181,12 @@ export class MapComponent implements OnInit {
     this.vectorSource.addFeature(marker);
   }
 
-  ngOnInit(): void {
-    this.initializeMap();
-    this.getPairs();
-  }
   private showModal(): void{
     $(this.modal.nativeElement).modal('show');
   }
 
-  private initializeMap(): void{
+  // Builds map and adds all required components to make the map work
+  private initializeMap(): void {
     this.player = new Feature({
       geometry: new Point(fromLonLat([24.6675, 59.39503])),
       name: 'player'
@@ -193,36 +225,23 @@ export class MapComponent implements OnInit {
       },
       projection: this.view.getProjection(),
     });
-    this.getlocation();
-
+  }
+  // Main function that checks if player clicks on markers
+  private updateMap(): void {
+    this.getPlayerLocation();
     this.map.on('click', (data) => {
-      if (this.amount === 0){
+      if (this.amount === 0) {
         this.quizopen = false;
         this.showModal();
-      }else{
-        this.map.forEachFeatureAtPixel(data.pixel, (feature, layer) => {
+      } else {
+        this.map.forEachFeatureAtPixel(data.pixel, (feature) => {
           const id = feature.get('QuestionId');
           console.log(this.amount);
-          if (id !== undefined && id !== -1){
-            this.getQuestion(id);
-            if (this.questionobj !== undefined){
-              this.question = this.questionobj.question;
-              this.answer = this.questionobj.answer;
-              this.choices = [];
-              this.questionobj.choices.forEach(q => {
-                this.choices.push(q);
-              });
-              this.choices = this.questionobj.choices;
-              this.points = this.questionobj.points;
-              console.log(this.choices);
-              console.log(this.answer);
-              this.showModal();
-              this.amount--;
-              this.vectorSource.removeFeature(feature);
-            }
+          if (id !== undefined && id !== -1) {
+            this.getQuestion(id, feature);
           }
         });
-     }
+      }
     });
   }
 }
