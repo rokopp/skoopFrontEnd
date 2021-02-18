24cd19a90a943b0b5a3b4fbe0108d927f9ddcbb8
Robin-Kevin Koppa <rokopp@ttu.ee>
2021-02-18 19:28:46 +0200

    Text changes according to screen width

--- src/app/components/questionsanswers/questionsanswers.component.ts
+++ src/app/components/questionsanswers/questionsanswers.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, HostListener, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-questionsanswers',
@@ -9,7 +9,21 @@ export class QuestionsanswersComponent implements OnInit {
 
   constructor() { }
 
+  public innerWidth: any;
   ngOnInit(): void {
+    this.innerWidth = window.innerWidth;
   }
 
+  @HostListener('window:resize', ['$event'])
+  onResize(event): void {
+    this.innerWidth = window.innerWidth;
+    console.log(this.innerWidth);
+  }
+
+  setClassNames(): string {
+    if (this.innerWidth < 990) {
+      return 'col-10 ml-3 mt-3';
+    }
+    return 'col-12 ml-5 mt-5';
+  }
 }
