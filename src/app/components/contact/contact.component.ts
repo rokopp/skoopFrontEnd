1e6d473c0e6ceab50dacc2683d21aedba00a2791
Robin-Kevin Koppa <rokopp@ttu.ee>
2021-02-18 19:57:10 +0200

    Added new dynamically changable backgrounds

--- src/app/components/contact/contact.component.ts
+++ src/app/components/contact/contact.component.ts
@@ -1,13 +1,14 @@
 import { Component, OnInit } from '@angular/core';
+import {BackgroundImageChangeService} from '../../services/background-image-change.service';
 
 @Component({
   selector: 'app-contact',
   templateUrl: './contact.component.html',
-  styleUrls: ['./contact.component.css']
+  styleUrls: ['./contact.component.css', '../../app.component.css']
 })
 export class ContactComponent implements OnInit {
 
-  constructor() { }
+  constructor(public bgService: BackgroundImageChangeService) { }
 
   ngOnInit(): void {
   }
