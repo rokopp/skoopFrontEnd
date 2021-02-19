cd6bd95f2b72c72dfc0da0e3554d71e545aceeb4
Robin-Kevin Koppa <rokopp@ttu.ee>
2021-02-19 13:21:57 +0200

    Added new url for oauth

--- src/app/components/oauth/oauth.ts
+++ src/app/components/oauth/oauth.ts
@@ -1,10 +1,12 @@
 export const OAuthSettings = {
-  // appId: '78637df6-bd9c-4013-9a4e-e935736bcf92',
-  // redirectUri: 'https://skoop.cs.taltech.ee/login/oauth2/code/azure',
-  // tenantID: 'https://login.microsoftonline.com/3efd4d88-9b88-4fc9-b6c0-c7ca50f1db57',
-  appId: 'd8f854c2-0de3-4ea6-a62f-8eee48c1b822',
-  redirectUri: 'https://skoop.cs.taltech.ee/api/login/oauth2/code/azure',
-  tenantID: 'https://login.microsoftonline.com/e90ec27d-08af-4b1b-bf3b-5c3c783c2424',
+  // TalTech
+  appId: '78637df6-bd9c-4013-9a4e-e935736bcf92',
+  redirectUri: 'https://skoop.cs.taltech.ee/api/login',
+  tenantID: 'https://login.microsoftonline.com/3efd4d88-9b88-4fc9-b6c0-c7ca50f1db57',
+  // Local
+  // appId: 'd8f854c2-0de3-4ea6-a62f-8eee48c1b822',
+  // redirectUri: 'https://skoop.cs.taltech.ee/api/login/oauth2/code/azure',
+  // tenantID: 'https://login.microsoftonline.com/e90ec27d-08af-4b1b-bf3b-5c3c783c2424',
   scopes: [
     'openid',
     'user.read',
