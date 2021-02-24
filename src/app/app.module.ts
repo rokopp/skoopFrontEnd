b67150870eca014019930a4aafa38137c6323bce
Robin-Kevin Koppa <rokopp@ttu.ee>
2021-02-24 20:21:46 +0200

    Missing imports

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -24,14 +24,46 @@ import { CreateMapComponent } from './components/create-map/create-map.component
 import { LocationSetsComponent } from './components/location-sets/location-sets.component';
 import { QuestionSetsComponent } from './components/question-sets/question-sets.component';
 import {HTTP_INTERCEPTORS, HttpClientModule} from '@angular/common/http';
-import { OAuthSettings } from './components/oauth/oauth';
 import {CommonModule} from '@angular/common';
 import { SimpleNotificationsModule } from 'angular2-notifications';
+import {
+  MSAL_CONFIG, MSAL_CONFIG_ANGULAR,
+  MsalService, MsalModule, MsalAngularConfiguration, MsalInterceptor
+} from '@azure/msal-angular';
+import {OAuthSettings} from './components/oauth/oauth';
+import {Configuration} from 'msal';
+
+
+export const protectedResourceMap: [string, string[]][] = [
+  ['https://graph.microsoft.com/beta/', ['user.read']]
+];
+
+function MSALConfigFactory(): Configuration {
+  return {
+    auth: {
+      clientId: OAuthSettings.appId,
+      authority: OAuthSettings.tenantID,
+      validateAuthority: true,
+      redirectUri: OAuthSettings.redirectUri,
+      postLogoutRedirectUri: 'https://skoop.cs.taltech.ee/avaleht',
+      navigateToLoginRequestUrl: true,
+    },
+    cache: {
+      storeAuthStateInCookie: false,
+    }
+  };
+}
+
+function MSALAngularConfigFactory(): MsalAngularConfiguration {
+  return {
+    popUp: true,
+    protectedResourceMap
+  };
+}
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent,
     NavbarComponent,
     QuestionsanswersComponent,
     GreetingComponent,
