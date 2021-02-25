ab7d5dc4e18b83b1e02aad765eb6af068f0e5c92
Robin-Kevin Koppa <rokopp@ttu.ee>
2021-02-25 13:29:54 +0200

    go play rooms has bg now, maybe need to change it

--- src/app/components/go-play-rooms/go-play-rooms.component.ts
+++ src/app/components/go-play-rooms/go-play-rooms.component.ts
@@ -1,9 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import {BackgroundImageChangeService} from "../../services/background-image-change.service";
 
 @Component({
   selector: 'app-go-play-rooms',
   templateUrl: './go-play-rooms.component.html',
-  styleUrls: ['./go-play-rooms.component.css']
+  styleUrls: ['./go-play-rooms.component.css', '../../app.component.css']
 })
 export class GoPlayRoomsComponent implements OnInit {
   teams: Array<any> = [
@@ -20,7 +21,7 @@ export class GoPlayRoomsComponent implements OnInit {
       name: 'China',
     }
   ];
-  constructor() { }
+  constructor(public bgService: BackgroundImageChangeService) { }
 
   ngOnInit(): void {
   }
