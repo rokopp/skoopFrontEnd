bd3e2eb0c06e6b7cdcddac65121b906f1d7cc102
Robin-Kevin Koppa <rokopp@ttu.ee>
2021-03-12 11:49:22 +0200

    Added Splash screen, but it displays all time when visiting greeting page

---
+++ src/app/components/splash-screen/splash-screen.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-splash-screen',
+  templateUrl: './splash-screen.component.html',
+  styleUrls: ['./splash-screen.component.css']
+})
+export class SplashScreenComponent implements OnInit {
+  windowWidth: string;
+  showSplash = true;
+  constructor() { }
+
+  ngOnInit(): void {
+    setTimeout(() => {
+      this.windowWidth = '-' + window.innerWidth + 'px';
+
+      setTimeout(() => {
+        this.showSplash = !this.showSplash;
+      }, 500);
+    }, 2000);
+  }
+}
