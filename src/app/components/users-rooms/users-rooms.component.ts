001d53c8b96013035f62e8029fe99521e3732a84
Robin-Kevin Koppa <rokopp@ttu.ee>
2021-03-17 13:30:37 +0200

    Changed hover color, added pagination design looking better

--- src/app/components/users-rooms/users-rooms.component.ts
+++ src/app/components/users-rooms/users-rooms.component.ts
@@ -30,7 +30,7 @@ export class UsersRoomsComponent implements OnInit {
     console.log(this.rooms);
 
     this.curPage = 1;
-    this.pageSize = 5;
+    this.pageSize = 7;
   }
   getRooms(): void{
     this.roomservice.getRooms().subscribe(room => {
